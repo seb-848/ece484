61a9cad797eae8722fbd23d0b999c0faf6662038
roprather <roprather@gmail.com>
2025-02-04 15:11:33 -0500

    Update main_speedtest.cpp

--- ece485pipe/main_speedtest.cpp
+++ ece485pipe/main_speedtest.cpp
@@ -1,19 +1,87 @@
-/**
- *
- */
-
 #include <iostream>
-#include <cstdlib>
-//#include <string>
-//#include <bits/stdc++.h>
-#include <fstream>
 #include <vector>
-#include "prng.h"
+#include <random>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <bits/stdc++.h>
+int const BITS = 8;
+string const GIVEN_FILE = "test_bits.txt";
 
-using namespace std;
+int parityXOR(int (&b)[BITS], int (&p)[BITS]) {
+    int a[BITS];
 
-// one index at a time
+    for(int i = 0; i < BITS; i++) {
+        a[i] = b[i] & p[i];
+    }
+    int result = a[0];
+    for (int i = 1; i < BITS; i++) {
+        result ^= a[i];
+    }
+    return result;
+}
+
+/**
+ * shifts the array to the right and adds new value from xor
+ * use after encrypting bit
+ * @param b array of B bits
+ * @param newValue xor value
+ * @return new array
+ */
+void shiftRegister(int (&b)[BITS], int (&p)[BITS]) {
+    int xorValue = parityXOR(b, p);
+    int n = sizeof(b)/sizeof(b[0]);
+    int d = BITS - 1;
+
+    rotate(b, b + n - d, b + n);
+    b[0] = xorValue;
+}
+
+/**
+ * take the last bit in b and xor with the info to be encrypted
+ * use before shifting
+ * @param b array of B
+ * @param infoToEncrypt info to be encrypted
+ * @return encryptedValue
+ */
+int encryption(int (&b)[BITS], int infoToEncrypt) {
+    int encryptionValue = b[BITS - 1];
+    int encryptedValue = infoToEncrypt ^ encryptionValue;
+    return encryptedValue;
+}
+std::vector<bool> generateRandomBits(size_t numBits) {
+    std::vector<bool> bits(numBits);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 1);
+
+    for (size_t i = 0; i < numBits; ++i) {
+        bits[i] = dis(gen);
+    }
+    return bits;
+}
+
+void saveBitsToFile(const std::vector<bool>& bits, const std::string& filename) {
+    std::ofstream outFile(filename);
+    if (!outFile) {
+        std::cerr << "Error opening file for writing!" << std::endl;
+        return;
+    }
+
+    for (bool bit : bits) {
+        outFile << (bit ? '1' : '0');
+    }
+
+    outFile.close();
+    std::cout << "Bits successfully written to " << filename << std::endl;
+}
+
 int main() {
+    size_t numBits = 8388608;  // 1 MB test
+    auto bits = generateRandomBits(numBits);
+
+    saveBitsToFile(bits, "test_bits.txt");
+
     srand((unsigned) time(nullptr));
     int rand_bits[BITS];
     int parity_bits[BITS];
@@ -28,7 +96,7 @@ int main() {
     vector<int> encrypted;
 
     // open the given file
-    fstream readfile(GIVEN_FILE, ios:: binary | ios:: in);
+    fstream readfile("test_bits.txt", ios:: binary | ios:: in);
 
     // char variable to take in the given file
     char c;
@@ -109,3 +177,5 @@ int main() {
 //        newreadfile.seekg(0);
     return 0;
 }
+
+
